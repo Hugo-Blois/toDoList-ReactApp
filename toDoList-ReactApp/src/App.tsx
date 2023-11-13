47ef14442df6c3ff94274aaa035cfbaae81b7c93
hugobls49 <hugoblois49@gmail.com>
2023-11-13 10:44:01 +0100

    Suppression parties inutiles

--- toDoList-ReactApp/src/App.tsx
+++ toDoList-ReactApp/src/App.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import List from './List';
 import Button from './Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [age, setAge] = useState(0)
 
   const itemList = [
     { id: 1, content: 'Item 1' },
@@ -13,36 +10,8 @@ function App() {
     { id: 3, content: 'Item 3' },
   ];
 
-  useEffect(() => {
-    console.log("la nouvelle valeur du compteur est: " + count)
-  }, [count]);
-  
-  function onChangeAge(e: React.ChangeEvent<HTMLInputElement>){
-    const value = Number(e.currentTarget.value)
-    if(!isNaN(value)){
-      setAge(value);
-      console.log(age);
-    }
-  }
-  
-  function verifyAge(){
-    if(age >= 18) console.log("Vous êtes majeur")
-    else console.log("Vous êtes mineur")
-  }
-
   return (
     <>
-      <div>Compteur: {count}</div>
-      <div>
-        <Button label="+" onClick={() => 
-          setCount((count) => count + 1)}/>
-        <Button label="-" onClick={() => 
-          setCount((count) => count - 1)}/>
-        <Button label="Reset" onClick={() => 
-          setCount(0)}/>
-      </div>
-      <input type="number" placeholder='Age' value={age} onChange={ onChangeAge }></input>
-      <Button label='Vérifier' onClick={verifyAge} />
       <div className="App">
       <h1>List Example</h1>
       <List items={itemList} />

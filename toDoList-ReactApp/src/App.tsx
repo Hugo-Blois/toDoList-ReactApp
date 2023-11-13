e1d1a1097f172569dfdd69312648b65f3a754533
Bastien <bastien.hugel@reseau.eseo.fr>
2023-11-13 11:04:28 +0100

    add task

--- toDoList-ReactApp/src/App.tsx
+++ toDoList-ReactApp/src/App.tsx
@@ -4,29 +4,31 @@ import List from './List';
 import Button from './Button'
 
 function App() {
+  const initialItemList = [
+    { id: 1, content: 'Item 1' },
+    { id: 2, content: 'Item 2' },
+    { id: 3, content: 'Item 3' },
+  ];
 
-  const [tache, setTache] = useState<string>();
+  const [tache, setTache] = useState<string>('');
+  const [itemList, setItemList] = useState(initialItemList);
 
   function onChangeTache(e: React.ChangeEvent<HTMLInputElement>){
     const text = String(e.currentTarget.value);
     if(text != '')
       setTache(text);
   }
-  const itemList = [
-    { id: 1, content: 'Item 1' },
-    { id: 2, content: 'Item 2' },
-    { id: 3, content: 'Item 3' },
-  ];
 
-
-  function addTache(){
-    console.log(tache)
+  function addTache(tache: string) {
+    const newItemList = [...itemList, { id: itemList.length + 1, content: tache }];
+    
+    setItemList(newItemList);
   }
 
   return (
     <>
       <input type="text" placeholder='Ajouter une nouvelle tâche' value={tache} onChange={ onChangeTache }></input>
-      <Button label='Ajouter' onClick={addTache} />
+      <Button label='Ajouter' onClick={() => addTache(tache)} />
       <div className="App">
       <h1>List Example</h1>
       <List items={itemList} />

859a2a6d5b67f4d87db7542b541b1758c2849a5b
hugobls49 <hugoblois49@gmail.com>
2023-11-20 08:45:28 +0100

    maj avec le dev

--- toDoList-ReactApp/src/App.tsx
+++ toDoList-ReactApp/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
 import List from './List';
-import Button from './Button'
+import Button from './Button';
 import { Fragment, useState } from 'react';
 
 interface ListItem {
   id: number;
+  title: string;
   content: string;
 }
 
@@ -12,17 +13,23 @@ function App() {
   const [addTask, setAddTask] = useState<boolean>(false);
   const [itemList, setItemList] = useState<ListItem[]>([]);
   const [tache, setTache] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   function onChangeTache(e: React.ChangeEvent<HTMLInputElement>){
     const text = String(e.currentTarget.value);
     if(text != '') setTache(text);
   }
+  function onChangeDescription(e: React.ChangeEvent<HTMLInputElement>){
+    const text = String(e.currentTarget.value);
+    if(text != '') setDescription(text);
+  }
 
-  function addTache(tache: string) {
+  function addTache(tache: string, description: string) {
     if (tache != ""){
-      const newItemList = [...itemList, { id: itemList.length + 1, content: tache }];
+      const newItemList = [...itemList, { id: itemList.length + 1, title: tache, content: description }];
       setItemList(newItemList);
-      setTache("");
+      setTache('');
+      setDescription('');
     }
   }
 
@@ -40,23 +47,25 @@ function App() {
 
   return (
     <div className="App">
-
       <h1>List Example</h1>
       <List items={itemList} onDelete={deleteTache} onEdit={editTache}/>
       {
        addTask === true ?
        <Fragment>
-       <input type="text" placeholder='Ajouter une nouvelle tâche' value={tache} onChange={ onChangeTache }></input>
-       <Button label='Confirm' onClick={() => {
-          setAddTask(false);
-          addTache(tache); 
-        }} />
+        <div className='add-task'>
+        <input type="text" placeholder='Titre' value={tache} onChange={ onChangeTache }></input>
+        <input type="text" placeholder='Description' value={description} onChange={ onChangeDescription }></input>
+        <Button label='Confirm' onClick={() => {
+                setAddTask(false);
+                addTache(tache, description);
+              } } children={undefined} />
+        </div>
        </Fragment>
         :
-        <Button label='Add a task' onClick={ () => setAddTask(true)}/>
+        <Button label='Add a task' onClick={() => setAddTask(true)} children={undefined}/>
       }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- toDoList-ReactApp/src/Button.tsx
+++ toDoList-ReactApp/src/Button.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Button.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Assurez-vous d'importer FontAwesomeIcon et les icônes nécessaires
+
 
 interface ButtonProps {
   label: string;
   onClick: () => void;
+  children: ReactNode; // Utilisez ReactNode pour accepter n'importe quel type d'enfant
 }
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
   return (
-    <button className="button" onClick={props.onClick}>{props.label}</button>
+    <button className="button" onClick={props.onClick}>
+      {props.label}
+      {props.children}
+      </button>
   );
 };
 

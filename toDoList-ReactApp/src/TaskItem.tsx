8b857c89d01da63071e283856ca2fb5f1b74d1d4
hugobls49 <hugoblois49@gmail.com>
2023-12-04 09:43:24 +0100

    Correction des erreurs

--- toDoList-ReactApp/src/TaskItem.tsx
+++ toDoList-ReactApp/src/TaskItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 interface ListItem {
     id: number;
@@ -12,56 +12,64 @@ interface ListItem {
 }
 
 interface TaskItemProps {
-    item: ListItem;
-    onToggleDone: (id: number) => void;
-    onDelete: (id: number) => void;
-    onEdit: (id: number, newTitlte: string, newContent: string) => void;
-  }
+  item: ListItem;
+  onToggleDone: (id: number) => void;
+  onDelete: (id: number) => void;
+  onEdit: (id: number, newTitle: string, newContent: string) => void;
+}
 
-  const TaskItem: React.FC<TaskItemProps> = ({ item, onDelete, onToggleDone }) => {
-  
-    return (
-      <li
-        key={item.id}
-        className="list-item"
-        style={{
-          border: '2px',
-          borderRadius: '8px',
-          backgroundColor: 'rgba(0, 0, 0, 0.2)',
-          padding: '10px',
-          marginBottom: '10px',
-          width: '70vw',
-          display: 'flex',
-          justifyContent: 'space-between', 
-          alignItems: 'center',
-        }}
-      >
-  
-        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '5px' }}>
-          <input
-            type="checkbox"
-            checked={item.done}
-            onChange={() => onToggleDone(item.id)}
-            style={{ marginRight: '30px' }}
-          />
+const TaskItem: React.FC<TaskItemProps> = ({ item, onDelete, onToggleDone, onEdit }) => {
 
-        </div>
+  return (
+    <li
+      key={item.id}
+      className="list-item"
+      style={{
+        border: '2px',
+        borderRadius: '8px',
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        padding: '10px',
+        marginBottom: '10px',
+        width: '70vw',
+        display: 'flex',
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+      }}
+    >
+
+      <div style={{ display: 'flex', alignItems: 'center', marginLeft: '5px' }}>
+        <input
+          type="checkbox"
+          checked={item.done}
+          onChange={() => onToggleDone(item.id)}
+          style={{ marginRight: '30px' }}
+        />
         
-  
-        <div>
-          {item.dueDate && (
-              <span className="item-due-date" >
-                  DeadLine : {item.dueDate}
-              </span>
-          )}
-  
+        <span className="item-title" style={{ marginRight: '50px', textDecoration: item.done ? 'line-through' : 'none' }}>
+          {item.title}</span>
+        <span className="item-content" style={{ textDecoration: item.done ? 'line-through' : 'none' }}>
+          {item.content}</span>
+      </div>
+      
+
+      <div>
+        {item.dueDate && (
+            <span className="item-due-date" >
+                DeadLine : {item.dueDate}
+            </span>
+        )}
+
+        <Button onClick={() => onEdit(item.id, item.title, item.content)} label={''}>
+          <FontAwesomeIcon icon={faEdit} />
+        </Button>
+
         <Button onClick={() => onDelete(item.id)} label={''}>
           <FontAwesomeIcon icon={faTrash} />
         </Button>
-        </div>
-  
-      </li>
-    );
-  };
-  
-  export default TaskItem;
+      </div>
+
+    </li>
+  );
+};
+
+export default TaskItem;
